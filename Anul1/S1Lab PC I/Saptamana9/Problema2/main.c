b98867a6dd99fea51bcaca556a74bb0d521e9a25
Matei Sirghe <matei.sirghe@gmail.com>
2024-11-26 15:10:45 +0200

    lab pc9

--- Anul1/S1Lab PC I/Saptamana9/Problema2/main.c
+++ Anul1/S1Lab PC I/Saptamana9/Problema2/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **a,**b;
+int **a,**b,c[100][100];
 int n1,n2,m1,m2;
 
 int main()
@@ -30,23 +30,51 @@ int main()
         *(b+i) = (int*) malloc(sizeof(int) * (n2-i));
         for(int j=0;j<i;j++){
             int x;
-            fscanf(fin,"%d",&x);
+            fscanf(fin,"%d ",&x);
         }
         for(int j=i;j<n2;j++){
-            fscanf(fin,"%d",&b[i][j]);
+            fscanf(fin,"%d",&b[i][j-i]);
+            ///printf("%d ",b[i][j]);
+        }
+    }
+
+    for(int i=0;i<n2;i++){
+        for(int h=0;h<m2;h++){
+            for(int j=0;j<m1;j++){
+                int par1,par2;
+                if(j>i || j>h){
+                    par1=0;
+                    par2=0;
+                }
+                else{
+                    par1=a[i][j];
+                    par2=b[j][h-j];
+                }
+                printf("%d %d\n",par1,par2);
+                c[i][h]+=par1*par2;
+            }
         }
     }
 
     ///Afisare a si b
+    fprintf(fout,"Matricea A:\n");
     for(int i=0;i<n1;i++){
         for(int j=0;j<i+1;j++){
             fprintf(fout,"%d ",a[i][j]);
         }
         fprintf(fout,"\n");
     }
+    fprintf(fout,"Matricea B:\n");
     for(int i=0;i<n2;i++){
         for(int j=0;j<n2-i;j++){
             fprintf(fout,"%d ",b[i][j]);
+        }
+        fprintf(fout,"\n");
+    }
+    fprintf(fout,"Matricea A*B:\n");
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<m2;j++){
+            fprintf(fout,"%d ",c[i][j]);
         }
         fprintf(fout,"\n");
     }
