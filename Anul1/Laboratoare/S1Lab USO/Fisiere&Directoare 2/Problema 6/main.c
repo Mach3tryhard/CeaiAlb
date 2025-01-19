23a178ed1bd08fde1a4ca0fa0f019a98c2e32303
Matei Sirghe <matei.sirghe@gmail.com>
2025-01-19 16:43:19 +0200

    a

---
+++ Anul1/Laboratoare/S1Lab USO/Fisiere&Directoare 2/Problema 6/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int main(int argc,char *argv[]){
+    FILE* fin=fopen(argv[1],"r");
+
+    int pipefd[2];
+    pipe(pipefd);
+
+    char v[1024];
+    char c;
+    int it=0;
+    while(c=fgetc(fin)!=EOF){
+        v[it]=c;
+        it+=1;
+    }
+
+    int a=atoi(argv[2]);
+    int b=atoi(argv[3]);
+    if(a!=b){
+        for(int i=0;i<=1;i++){
+        pid_t pid = fork();
+            if (pid == -1) {
+                perror("fork a murit");
+                return -1;
+            }
+            if (pid == 0) {
+                close(pipefd[0]);
+
+                char *program = argv[0];
+                char *args[4];
+                args[0] = argv[0];
+                args[1] = argv[1];
+                if(i==0){
+                    sprintf(args[2], "%d", a);
+                    sprintf(args[3], "%d", b/2);
+                    //args[2]=a;
+                    //args[3]=b/2;
+                }
+                if(i==1){
+                    sprintf(args[2], "%d", b/2);
+                    sprintf(args[3], "%d", b);
+                    //args[2]=b/2;
+                    //args[3]=b;
+                }
+                args[4]=NULL;
+                printf("%s %s program cu acest ab \n",args[2],args[3]);
+                execve(program, args, NULL); 
+
+                perror("execve a murit");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    
+
+    if(a==b){
+        char str[b-a+1];
+        for(int i=a;i<=b;i++){
+            str[i-a]=v[i];
+        }
+        write(pipefd[1],str,strlen(str));
+    }
+    else{
+        char buffer[1024];
+		ssize_t bytesRead;
+        /*while((bytesRead=read(pipefd[0],buffer,sizeof(buffer)-1))>0){
+			buffer[bytesRead]='\0';
+			printf("%s",buffer);
+		}*/
+    }
+
+    return 0;
+}
